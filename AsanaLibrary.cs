147e2ab99423e0ec1b182b17054ee1e33c163e55
Bruce M. Axtens <bugmagnet@outlook.com>
2020-10-01 10:28:23 +0800

    Add project files.

---
+++ AsanaLibrary.cs
@@ -0,0 +1,93 @@
+﻿using RestSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class AsanaLibrary
+{
+    public  List<string> Trap = new List<string>();
+    internal  INI Ini;
+    internal  string apiUrl = "https://app.asana.com/api/1.0";
+    internal  Dictionary<string, object> queryDict = new Dictionary<string, object>();
+    internal  Dictionary<string, object> paramDict = new Dictionary<string, object>();
+    internal  string token;
+
+    public AsanaLibrary(string ini)
+    {
+        Ini = new INI(ini);
+        token = Ini.IniReadValue("Authorisation", "Token", string.Empty);
+    }
+
+    public  string Do(string method, string param)
+    {
+        {
+            if (Trap.FindIndex(e => e == MethodBase.GetCurrentMethod().Name) > -1)
+            {
+                System.Diagnostics.Debugger.Launch();
+            }
+
+            var answer = ReplaceFromDict(param, queryDict);
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest(answer)
+            {
+                Method = (Method)Enum.Parse(typeof(Method), method)
+            };
+            request.AddHeader("Authorization", "Bearer " + token);
+
+            foreach (var kvp in paramDict)
+            {
+                request.AddParameter(kvp.Key, kvp.Value, ParameterType.GetOrPost);
+            }
+            var response = client.Execute(request);
+            return response.Content;
+        }
+    }
+    public  string Get(string param) => Do("GET", param);
+    
+    public  string Put(string param) => Do("PUT", param);
+    
+    public  string Post(string param) => Do("POST", param);
+    
+    public  string Delete(string param) => Do("DELETE", param);
+
+    public  void SetQueryDict(string name, object value)
+    {
+        if (Trap.FindIndex(e => e == MethodBase.GetCurrentMethod().Name) > -1)
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+
+        queryDict[name] = value;
+    }
+
+    public  void SetParamDict(string name, object value)
+    {
+        if (Trap.FindIndex(e => e == MethodBase.GetCurrentMethod().Name) > -1)
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+
+        paramDict[name] = value;
+    }
+
+    public  void ClearParamDict() => paramDict.Clear();
+
+    public  void ClearQueryDict() => queryDict.Clear();
+    
+    public  string ReplaceFromDict(string pattern, Dictionary<string, object> dso)
+    {
+        if (Trap.FindIndex(e => e == MethodBase.GetCurrentMethod().Name) > -1)
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+
+        string answer = pattern;
+        foreach (var kvp in dso)
+        {
+            answer = answer.Replace("{" + kvp.Key + "}", kvp.Value.ToString());
+        }
+        return answer;
+    }
+}
+
---
+++ INI.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class INI
+{
+    public string path;
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool WritePrivateProfileString(
+        string lpAppName,
+        string lpKeyName,
+        string lpString,
+        string lpFileName);
+
+    // [DllImport("kernel32", CharSet = CharSet.Unicode)]
+    // private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+    static extern uint GetPrivateProfileString(
+        string lpAppName,
+        string lpKeyName,
+        string lpDefault,
+        StringBuilder lpReturnedString,
+        uint nSize,
+        string lpFileName);
+    //[DllImport("kernel32", CharSet = CharSet.Unicode)]
+    //private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+    /// <summary>
+    /// INIFile Constructor.
+    /// </summary>
+    /// <param name="INIPath"></param>
+    public INI(string INIPath) => path = INIPath;
+
+    /// <summary>
+    /// Write Data to the INI File
+    /// </summary>
+    /// <param name="Section"></param>
+    /// Section name
+    /// <param name="Key"></param>
+    /// Key Name
+    /// <param name="Value"></param>
+    /// Value Name
+    public void IniWriteValue(string Section, string Key, string Value)
+    {
+        WritePrivateProfileString(Section, Key, Value, this.path);
+    }
+
+    /// <summary>
+    /// Read Data Value From the Ini File
+    /// </summary>
+    /// <param name="Section"></param>
+    /// <param name="Key"></param>
+    /// <param name="Path"></param>
+    /// <returns></returns>
+    public string IniReadValue(string Section, string Key, string DefaultValue)
+    {
+        StringBuilder temp = new StringBuilder(1024);
+        uint i = GetPrivateProfileString(Section, Key, "", temp, 1024, this.path);
+        if (i != 0)
+            return temp.ToString();
+        else
+            return DefaultValue;
+
+    }
+}
